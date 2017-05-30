5786d2eb34ebbb73f0f47dd4642482d83fe0141f
Your Name <you@example.com>
2017-05-30 20:26:20 +0200

    fix conflict

--- mean-app/routes/index.js
+++ mean-app/routes/index.js
@@ -1,19 +1,19 @@
-<<<<<<< HEAD
-var router = require('express').Router();
-var tagRoutes = require('./tag');
-var unitTypeRoutes = require('./unitType');
-var recipeRoutes=  require('./recipe');
-var reviewRoutes = require('./review') ;
-var userRoutes = require('./user') ;
+// <<<<<<< HEAD
+// var router = require('express').Router();
+// var tagRoutes = require('./tag');
+// var unitTypeRoutes = require('./unitType');
+// var recipeRoutes=  require('./recipe');
+// var reviewRoutes = require('./review') ;
+// var userRoutes = require('./user') ;
 
 
-router.use("/api/tags", tagRoutes);
-router.use("/api/unitTypes", unitTypeRoutes);
-router.use("/api/recipe", recipeRoutes) ;
-router.use("/api/review", reviewRoutes) ;
-router.use("/api/user", userRoutes) ;
+// router.use("/api/tags", tagRoutes);
+// router.use("/api/unitTypes", unitTypeRoutes);
+// router.use("/api/recipe", recipeRoutes) ;
+// router.use("/api/review", reviewRoutes) ;
+// router.use("/api/user", userRoutes) ;
 
-=======
+// =======
 var router = require('express').Router();
 var tagRoutes = require('./tag');
 var unitTypeRoutes = require('./unitType');
@@ -31,5 +31,5 @@ router.use("/api/user", userRoutes);
 router.use("api/badge", badgeRoutes);
 router.use("api/ingredient", ingredientRoutes);
 
->>>>>>> a684d1849596a302decc9ae839652321f281866d
+
 module.exports = router;
